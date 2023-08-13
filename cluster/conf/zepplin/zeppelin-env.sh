2ef33cee8d92b3a82a3124d09d1e27b3f92f5e67
김동현 <dbin67@naver.com>
2023-08-13 20:20:43 +0900

    2-9. 제플린 설정

---
+++ cluster/conf/zepplin/zeppelin-env.sh
@@ -0,0 +1,10 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/usr/local/hadoop
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_HOME=/usr/local/spark
+export SPARK_MASTER=yarn
+export ZEPPELIN_PORT=18888
+export PYTHONPATH=/usr/bin/python3
+export PYSPARK_PYTHON=/usr/bin/python3
+export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
