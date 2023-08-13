3d46ab7f6a3bf02e40925918aa3e495fa017f12a
김동현 <dbin67@naver.com>
2023-08-13 20:34:38 +0900

    2-11. 하이브 설정

---
+++ cluster/conf/hive/hive-env.sh
@@ -0,0 +1,6 @@
+export HIVE_HOME=/usr/local/hive
+export HIVE_CONF_DIR=/usr/local/hive/conf
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop
+export HIVE_AUX_JARS_PATH=$HIVE_AUX_JARS_PATH
